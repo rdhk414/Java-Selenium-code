e1a4e4c786d10b16d1731544eb66b88aae566684
rdhk414 <133076373+rdhk414@users.noreply.github.com>
2023-07-18 15:40:21 -0400

    New pattern

---
+++ pattern/Constr.java
@@ -0,0 +1,22 @@
+package const_homework;
+
+public class Constr {
+	
+	private int a, b;
+	
+ public Constr()
+ {
+	 this.a=2;
+	 this.b=4;
+	 System.out.println(a+b);
+ }
+	
+public Constr(int apass, int bpass)
+{
+	this.a =apass;
+	this.b=bpass;
+	
+	System.out.println(a+b);
+	
+}
+}
